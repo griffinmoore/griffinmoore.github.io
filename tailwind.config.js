04e01f6a1450abeaf3e3b242aea46d737f83e059
Griffin Moore <griffinmoore@gmail.com>
2025-04-22 15:26:20 -0400

    Visual update

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,8 +12,9 @@ module.exports = {
       colors: {
         'blue-500': '#5276FA',
         'blue-600': '#2448CC',
-        'gray-700': '#505050',
-        'gray-500': '#9D9D9D'
+        'brown-100': '#E7E0DA',
+        'gray-500': 'rgba(0,0,0,0.4)',
+        'gray-800': 'rgba(0,0,0,0.9)'
       }
     },
   },
